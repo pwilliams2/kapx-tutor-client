99559be11de63e324aeab559b384596681835a9e
Preston Williams <pwilliams2@kaplan.edu>
2015-02-05 18:54:14 -0500

    subscribe / unsubscribe behavior

--- static/js/tutor-client.js
+++ static/js/tutor-client.js
@@ -2,19 +2,19 @@
  * Created by admin on 1/27/15.
  */
 
-//var hangoutsBaseUrl = "https://plus.google.com/hangouts/_/";
 var hangoutsBaseUrl = "https://talkgadget.google.com/hangouts/_/"  //gid is appended below at run-time
 
-function launchClientHangout(gid, subject) {
-    var url = hangoutsBaseUrl + gid + "?gd=" + subject;
+function launchClientHangout(gid) {
+    var url = hangoutsBaseUrl + gid;
+    console.log('url: ' + url);
     window.open(url, "", "width=1002,height=700,location=0,menubar=0,scrollbars=1,status=1,resizable=0")
 }
 
 $(".launch-hangout").click(function () {
     gid = $(this).find(".gid").text();
-    subject = $(this).find(".subject").text();
-    console.log("gid: " + gid);
-    launchClientHangout(gid, subject);
+    //subject = $(this).find(".subject").text();
+    //console.log("gid: " + gid + " subject:" + subject);
+    launchClientHangout(gid);
 });
 
 function reload() {
