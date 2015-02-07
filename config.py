22af69ad87b3125aa55ae4d5b5c605bae36a51cc
Preston Williams <pwilliams2@kaplan.edu>
2015-02-07 09:13:07 -0500

    refactor

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
 base_path = os.path.dirname(__file__)
+LOCAL_HANGOUT_APP_URL = 'https://kx-tutor-hangout-app.appspot.com/subjects'
 DEV_HANGOUT_APP_URL = 'https://kx-tutor-hangout-app.appspot.com/subjects'
+PROD_HANGOUT_APP_URL = 'https://kx-tutor-hangout-app.appspot.com/subjects'
+
+#TODO: add some code to select the correct env url
+ENVIRONMENT_URL = DEV_HANGOUT_APP_URL
--- web/handlers.py
+++ web/handlers.py
@@ -1,29 +1,37 @@
 import json
+
 import httplib2
-from apiclient import discovery
+from google.appengine import runtime
 
 import config
 from lib.base import BaseHandler
-from utils import autolog, JSONEncoder
+from utils import autolog
 
 
 API_ROOT = 'https://kx-tutor-hangout-app.appspot.com/_ah/api'
 API_NAME = 'tutorhangouts'
 VERSION = 'v1'
 
+
 class MainPage(BaseHandler):
     def get(self):
-        template_data = {'subjects_query': self.get_subjects()}
-        self.render_template('views/student.html', **template_data)
 
+        subjects = self.get_subjects()
+        if subjects:
+            template_data = {'subjects_query': subjects}
+            self.render_template('views/student.html', **template_data)
+        else:
+            self.render_template('views/unavailable.html')
 
 
     def get_subjects(self):
         """ Retrieve available subjects        """
-        resp, content = httplib2.Http().request(config.DEV_HANGOUT_APP_URL)
-
-        if content:
-            return json.loads(content)
-        else:
-            autolog("no subjects found")
-            return [{}]
+        try:
+            resp, content = httplib2.Http().request(config.ENVIRONMENT_URL)
+            if content:
+                return json.loads(content)
+            else:
+                autolog("no subjects found")
+        except runtime.DeadlineExceededError:
+            autolog('Deadline exceed error on url: %s' % config.ENVIRONMENT_URL)
+        return [{}]
