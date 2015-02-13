a7ce33f9d1675e43f09b8b32e93788c05bb47e50
Preston Williams <pwilliams2@kaplan.edu>
2015-02-13 10:28:21 -0500

    refactoring

--- static/js/tutor-client.js
+++ static/js/tutor-client.js
@@ -5,7 +5,7 @@
 var hangoutsBaseUrl = "https://talkgadget.google.com/hangouts/_/"  //gid is appended below at run-time
 
 function launchClientHangout(gid, subject) {
-    var url = hangoutsBaseUrl + gid + '&gd=' + subject;
+    var url = hangoutsBaseUrl + gid + '?gd=' + subject;
     console.log('url: ' + url);
     window.open(url, "", "width=1002,height=700,location=0,menubar=0,scrollbars=1,status=1,resizable=0")
 }
