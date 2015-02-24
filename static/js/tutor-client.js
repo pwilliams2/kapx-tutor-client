f8ee6495759dc486a93f935db18bca7921e91518
Preston Williams <pwilliams2@kaplan.edu>
2015-02-24 17:11:03 -0500

    refactor to launch H-O from server

--- static/js/tutor-client.js
+++ static/js/tutor-client.js
@@ -4,9 +4,17 @@
 
 var hangoutsBaseUrl = "https://talkgadget.google.com/hangouts/_/"  //gid is appended below at run-time
 
+//function launchClientHangout(gid, subject) {
+//    var url = hangoutsBaseUrl + gid + '?gd=' + subject;
+//    console.log('url: ' + url);
+//    window.open(url, "", "width=1002,height=700,location=0,menubar=0,scrollbars=1,status=1,resizable=0")
+//}
+
 function launchClientHangout(gid, subject) {
-    var url = hangoutsBaseUrl + gid + '?gd=' + subject;
+    var url = "https://kx-tutor-hangout-app.appspot.com/hangouts?gid=" + gid + '&subject=' + subject;
+    //var url = "http://localhost:8080/hangouts?gid=" + gid + '&subject=' + subject;
     console.log('url: ' + url);
+    //window.location.href = url;
     window.open(url, "", "width=1002,height=700,location=0,menubar=0,scrollbars=1,status=1,resizable=0")
 }
 
@@ -22,6 +30,6 @@ function reload() {
 $(function () { //reload page 20 seconds
     setInterval(function () {
         reload();
-    }, 20000);
+    }, 60000);
 });
 
